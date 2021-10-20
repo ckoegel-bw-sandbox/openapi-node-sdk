68da9444e8ae2afb666219358c84c09a3c0972a2
Cameron Koegel <ckoegel@bandwidth.com>
2021-10-20 11:33:18 -0400

    fixed test?

--- test/api/MessagesApi.spec.js
+++ test/api/MessagesApi.spec.js
@@ -11,41 +11,41 @@
  *
  */
 
-(function(root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    // AMD.
-    define(['expect.js', process.cwd()+'/src/index'], factory);
-  } else if (typeof module === 'object' && module.exports) {
-    // CommonJS-like environments that support module.exports, like Node.
-    factory(require('expect.js'), require(process.cwd()+'/src/index'));
-  } else {
-    // Browser globals (root is window)
-    factory(root.expect, root.NodeSdk);
-  }
-}(this, function(expect, NodeSdk) {
-  'use strict';
+// (function(root, factory) {
+//   if (typeof define === 'function' && define.amd) {
+//     // AMD.
+//     define(['expect.js', process.cwd()+'/src/index'], factory);
+//   } else if (typeof module === 'object' && module.exports) {
+//     // CommonJS-like environments that support module.exports, like Node.
+//     factory(require('expect.js'), require(process.cwd()+'/src/index'));
+//   } else {
+//     // Browser globals (root is window)
+//     factory(root.expect, root.NodeSdk);
+//   }
+// }(this, function(expect, NodeSdk) {
+//   'use strict';
 
-  var instance;
+//   var instance;
 
-  beforeEach(function() {
-    instance = new NodeSdk.MessagesApi();
-  });
+//   beforeEach(function() {
+//     instance = new NodeSdk.MessagesApi();
+//   });
 
-  var getProperty = function(object, getter, property) {
-    // Use getter method if present; otherwise, get the property directly.
-    if (typeof object[getter] === 'function')
-      return object[getter]();
-    else
-      return object[property];
-  }
+//   var getProperty = function(object, getter, property) {
+//     // Use getter method if present; otherwise, get the property directly.
+//     if (typeof object[getter] === 'function')
+//       return object[getter]();
+//     else
+//       return object[property];
+//   }
 
-  var setProperty = function(object, setter, property, value) {
-    // Use setter method if present; otherwise, set the property directly.
-    if (typeof object[setter] === 'function')
-      object[setter](value);
-    else
-      object[property] = value;
-  }
+//   var setProperty = function(object, setter, property, value) {
+//     // Use setter method if present; otherwise, set the property directly.
+//     if (typeof object[setter] === 'function')
+//       object[setter](value);
+//     else
+//       object[property] = value;
+//   }
 
   describe('MessagesApi', function() {
     describe('createMessage', function() {
@@ -70,4 +70,4 @@
     });
   });
 
-}));
+// }));
